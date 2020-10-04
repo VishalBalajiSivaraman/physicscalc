66f742173b9a3f222fb79232a38812ffbdee778a
Vishal Balaji Sivaraman <64977199+The-SocialLion@users.noreply.github.com>
2020-10-04 16:00:57 +0530

    Update __init__.py

--- physicscalc/__init__.py
+++ physicscalc/__init__.py
@@ -1,1 +1,12 @@
+# Module name:electronicscalc
+# Short description: Physicscalc (or) Physics Calculator is a package that houses the  functions which can simplify ,solve any problem related to all the various concepts involved in physics and much more...!
+# Developers:  Vishal Balaji Sivaraman (@The-SocialLion) 
+# Contact email address: vb.sivaraman_official@yahoo.com 
+# Modules required:math
 
+# Command to install electronics-calc:
+# >>> pip install physicscalc
+
+# Essential modules
+import math as mt
+import vectoralg as vg
