f8a6af4c24260ad06b4b64cce7479c4dee41239b
Vishal Balaji Sivaraman <64977199+The-SocialLion@users.noreply.github.com>
2020-10-04 16:30:31 +0530

    included mean error fn

--- physicscalc/__init__.py
+++ physicscalc/__init__.py
@@ -12,11 +12,20 @@ import math as mt
 import numpy as np
 import vectoralg as vg
 
-def absolute_error(a,n):
-  A=np.array(a)
-  S=np.sum(A)
-  E=S/n
-  print("absolute error",E)
-  
+
+
+def meanabsolute_error(a,n):
+  if(len(a)==n):
+    b=[]
+    for i in a:
+      b.append(abs(i))
+    A=np.array(b)
+    S=np.sum(A)
+    M=S/n
+    print("mean absolute error",M)
+  else:
+    print("error try again")
+
+
  
 
